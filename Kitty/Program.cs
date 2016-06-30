30b4d30a56a2c6a6d9c45b8030f04bc8de2e8a75
Jarek Hirniak <j@hirniak.com>
2016-06-30 23:54:42 +0100

    Initial commit - added project files. Includes tray icon and move to (50, 50) code.
    Added tray icon mode by inheritance from ApplicationContext.
    
    Added Exit and ResetMousePosition (to 50, 50 of the main monitor) functionalities.

---
+++ Kitty/Program.cs
@@ -0,0 +1,56 @@
+﻿using Kitty.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kitty
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MyCustomApplicationContext());
+        }
+    }
+
+    public class MyCustomApplicationContext : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+
+        public MyCustomApplicationContext()
+        {
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Resources.AppIcon,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Catch cursor", ResetCursorPosition),
+                new MenuItem("Exit", Exit)
+            }),
+                Visible = true
+            };
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+
+            Application.Exit();
+        }
+
+        void ResetCursorPosition(object sender, EventArgs e)
+        {
+            Cursor.Position = new System.Drawing.Point(50, 50);
+        }
+    }
+
+}
